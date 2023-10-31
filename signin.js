8419b1e9d5eee06c87b12352b9f50b20ce695bbc
mohd-ayaan <143902353+mohd-ayaan@users.noreply.github.com>
2023-10-31 20:34:36 +0530

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,23 +1,21 @@
 $(document).ready(function() {
-    // Function to handle form submission
     $("form").submit(function(event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        // Get the values of the input fields
+        event.preventDefault(); // Prevent default form submission
+        
         const userName = $(".form-input[placeholder='User Name']").val();
         const password = $(".form-input[placeholder='Password']").val();
 
-        // You can add your login validation logic here
-        if (userName === "your_username" && password === "your_password") {
+        if (userName === "your_username" && password === "your_password") 
+        {
             // Replace 'your_username' and 'your_password' with actual credentials in database
-            alert("Login successful!"); // You can customize this part
-        } else {
-            alert("Invalid username or password."); // You can customize this part
+            alert("Login successful!");
+        } else 
+        {
+            alert("Invalid username or password.");
         }
     });
 
-    // Function to handle the "Reset" button
     $(".form-button:contains('Reset')").click(function() {
-        $("form")[0].reset(); // Reset the form fields
+        $("form")[0].reset();
     });
 });

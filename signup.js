1229348531afb6eb71bc08f33491ede786a0b782
mohd-ayaan <143902353+mohd-ayaan@users.noreply.github.com>
2023-10-31 20:27:42 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,27 +1,20 @@
 $(document).ready(function() {
     $("form").submit(function(event) {
-        event.preventDefault(); // Prevent the default form submission 
+        event.preventDefault(); // Prevent default submission 
         
-        // Get form input values
         const firstName = $("#firstName").val();
         const lastName = $("#lastName").val();
         const email = $("#email").val();
         const password = $("#password").val();
         const confirmPassword = $("#confirmPassword").val();
 
-        // You can add more validation here, e.g., checking if passwords match
         if (password !== confirmPassword) {
             alert("Passwords do not match");
             return;
         }
-
-        // You can add more validation as needed
-
         // If all validation passes, you can submit the form to a server or perform other actions
-        // For now, let's just display a success message
         alert("Form submitted successfully!");
-
-        // You can also reset the form if needed
+        
         $(this).trigger("reset");
     });
 });
